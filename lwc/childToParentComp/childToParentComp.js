0f8929a7c43ecd648011b75a17b348a3a2652477
lwccomponents <>
2024-06-09 21:08:07 +0000

    [ClickDeploy] Dev to Github Sync

---
+++ lwc/childToParentComp/childToParentComp.js
@@ -0,0 +1,13 @@
+import { LightningElement } from 'lwc';
+export default class ChildToParentComp extends LightningElement {
+textValue = '';
+
+handleChange(event){
+    this.textValue = event.target.value;
+    console.log(this.textValue);
+    const productDetails = new CustomEvent('sendinginvoicedetails',{
+     detail:this.textValue
+    });
+    this.dispatchEvent(productDetails);
+}
+}
